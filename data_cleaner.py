b599293ff102d400119e856f0f522fab47473bde
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:13:34 -0500

    validate date fields

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,1 +1,14 @@
 from utils import validate_date, validate_number
+
+
+def clean_weather_data(raw_records):
+    cleaned = []
+    for r in raw_records:
+        if (
+            validate_date(r["date"])
+        ):
+            cleaned.append({
+                "date": r["date"],
+                "city": r["city"]
+            })
+    return cleaned
