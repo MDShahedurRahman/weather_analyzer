ade4194a323bd118afbe067a59adf6a2f11b8e70
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:03:15 -0500

    add constructor

--- weather_record.py
+++ weather_record.py
@@ -1,1 +1,6 @@
 class WeatherRecord:
+    def __init__(self, date, city, temperature, humidity):
+        self.date = date
+        self.city = city
+        self.temperature = temperature
+        self.humidity = humidity
