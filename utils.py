a6edde6ada1af2a5e94e5912d2fbddcda493b88e
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:09:26 -0500

    validate date fields

--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
 from datetime import datetime
 from config import DATE_FORMAT
+
+
+def validate_date(date_str):
+    try:
+        datetime.strptime(date_str, DATE_FORMAT)
+        return True
+    except ValueError:
+        return False
