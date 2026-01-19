59a21be3c001a7e3886770fc6f5799be664e57f7
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:34:57 -0500

    add summary report builder

--- report.py
+++ report.py
@@ -1,1 +1,10 @@
 from analyzer import average_temperature, max_temperature, min_temperature
+
+
+def summary_report(records):
+    return {
+        "average_temp": average_temperature(records),
+        "max_temp": max_temperature(records),
+        "min_temp": min_temperature(records),
+        "total_records": len(records)
+    }
