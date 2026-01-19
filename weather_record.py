df2a20dba9c13e94d61321cf75e9055565487b0d
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:03:56 -0500

    add to_dict method

--- weather_record.py
+++ weather_record.py
@@ -4,3 +4,11 @@ class WeatherRecord:
         self.city = city
         self.temperature = temperature
         self.humidity = humidity
+
+    def to_dict(self):
+        return {
+            "date": self.date,
+            "city": self.city,
+            "temperature": self.temperature,
+            "humidity": self.humidity
+        }
