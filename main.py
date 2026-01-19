74df92612a800fe77f10730dc2e35ca9859da3cb
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:36:18 -0500

    create application entry point

---
+++ main.py
@@ -0,0 +1,11 @@
+from data_loader import load_weather_data
+from data_cleaner import clean_weather_data
+from analyzer import filter_by_city
+from report import summary_report, print_report
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
