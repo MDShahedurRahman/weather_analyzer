b998207c9de51aebb01c1ba2b24a0ae6a7959e60
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-18 23:07:26 -0500

    add load_weather_data method

--- data_loader.py
+++ data_loader.py
@@ -4,4 +4,9 @@ from config import DATA_FILE
 
 def load_weather_data():
     records = []
+    with open(DATA_FILE, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            records.append(row)
+
     return records
